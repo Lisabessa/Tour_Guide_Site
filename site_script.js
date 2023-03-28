bb6d1314c2b06bda7e73d62759e0220ff5434ecf
Lisabessa <75796619+Lisabessa@users.noreply.github.com>
2023-03-28 23:54:00 +0700

    Add validation

--- site_script.js
+++ site_script.js
@@ -1,7 +1,6 @@
 var form = document.querySelector("form");
-var log = document.querySelector("#log");
 
-const validateNameRegex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
+const validateNameRegex = /^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/;
 const validateEmailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ 
 const validatePhoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
 
@@ -10,9 +9,8 @@ form.addEventListener("submit", function(event){
     let name = "";
     let email = "";
     let phone = "";
-    let output = "";
+    
     for (const entry of data) {
-        output += entry[0] + "=" + entry[1] + "\r";
         if (entry[0] == 'userName'){
             name = entry[1];
         }
@@ -23,16 +21,15 @@ form.addEventListener("submit", function(event){
             phone = entry[1];
         }
     };
-    //log.innerText = output + name + email + phone;
     
-    //CheckInfo(name, email, phone);
+    CheckInfo(name, email, phone);
     event.preventDefault();
 }, false);
 
 function CheckInfo(name, email, phone){
 
     if(!validateNameRegex.test(name)){
-        alert('Incorrect name');
+       alert('Incorrect name');
     }
     else if (!validateEmailRegex.test(email)){
         alert('Incorrect email adress');
