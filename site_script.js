bb29e2981ac4a6cd1c34f4d258b946e0d1dc2ffd
Lisabessa <75796619+Lisabessa@users.noreply.github.com>
2023-03-28 18:19:29 +0700

    Fix submit event

--- site_script.js
+++ site_script.js
@@ -1,13 +1,35 @@
-function CheckInfo(){
-    let name=document.getElementById("userName").value;
-    let email = document.getElementById("userEmail").value;
-    let phone = document.getElementById("userPhone").value;
-    
-    const validateNameRegex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
-    const validateEmailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ 
-    const validatePhoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+var form = document.querySelector("form");
+var log = document.querySelector("#log");
 
-    alert("Hello world");
+const validateNameRegex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
+const validateEmailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/ 
+const validatePhoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+
+form.addEventListener("submit", function(event){
+    let data = new FormData(form);
+    let name = "";
+    let email = "";
+    let phone = "";
+    let output = "";
+    for (const entry of data) {
+        output += entry[0] + "=" + entry[1] + "\r";
+        if (entry[0] == 'userName'){
+            name = entry[1];
+        }
+        else if (entry[0] == 'userEmail'){
+            email = entry[1];
+        }
+        else if (entry[0] == 'userPhone'){
+            phone = entry[1];
+        }
+    };
+    //log.innerText = output + name + email + phone;
+    
+    //CheckInfo(name, email, phone);
+    event.preventDefault();
+}, false);
+
+function CheckInfo(name, email, phone){
 
     if(!validateNameRegex.test(name)){
         alert('Incorrect name');
